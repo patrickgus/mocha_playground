114e1b672affb9d05cd7ceebf4899ec9c622ba25
Patrick Gustafson <patrickgus@charter.net>
2019-12-24 09:44:50 -0800

    refactor to remove unnecessary code

--- test/divide.test.js
+++ test/divide.test.js
@@ -3,29 +3,12 @@ const expect = require("chai").expect;
 
 describe("Divide function", () => {
   it("should divide positive integers correctly", () => {
-    // define inputs
-    const a = 8;
-    const b = 4;
-    const expectedAnswer = 2;
-
-    // invoke the function
-    const actualAnswer = divide(a, b);
-
-    // assert that expected === actual
-    expect(actualAnswer).to.equal(expectedAnswer);
+    expect(divide(8, 4)).to.equal(2);
   });
 
   it("should throw an error when divide by zero", () => {
-    // define inputs
-    const a = 8;
-    const b = 0;
-
-    // set up the function call
-    const fn = () => {
-      divide(a, b);
-    };
-
-    // assert that exception is thrown
-    expect(fn).to.throw();
+    expect(() => {
+      divide(8, 0);
+    }).to.throw();
   });
 });
