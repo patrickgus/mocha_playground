65992f949d966349cc84bfb4649b445f35eeb909
Patrick Gustafson <patrickgus@charter.net>
2019-12-26 13:17:25 -0800

    comment out test

--- test/sort.test.js
+++ test/sort.test.js
@@ -1,11 +1,11 @@
-const sort = require("../sort");
-const expect = require("chai").expect;
+// const sort = require("../sort");
+// const expect = require("chai").expect;
 
-describe("Sort function", () => {
-  it("should sort the array in ascending order", () => {
-    const arr1 = sort([3, 2, 4, 1]);
-    const arr2 = [1, 2, 3, 4];
+// describe("Sort function", () => {
+//   it("should sort the array in ascending order", () => {
+//     const arr1 = sort([3, 2, 4, 1]);
+//     const arr2 = [1, 2, 3, 4];
 
-    expect(arr1).to.deep.equal(arr2);
-  });
-});
+//     expect(arr1).to.deep.equal(arr2);
+//   });
+// });
