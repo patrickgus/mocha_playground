610ce05c6be44173305e55dce02fe6cedfa67e33
Patrick Gustafson <patrickgus@charter.net>
2019-12-26 13:17:13 -0800

    add test to diff

--- test/arrayDiff.test.js
+++ test/arrayDiff.test.js
@@ -13,4 +13,8 @@ describe("Array diff", () => {
   it("should return correctly if some elements are removed", () => {
     expect(diff([1, 2, 3], [2])).to.eql([1, 3]);
   });
+
+  it("should remove 1 from [1,2,3]", () => {
+    expect(diff([1, 2, 3], [1])).to.not.include(1);
+  });
 });
