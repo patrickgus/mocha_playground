5bcd8d949a7f855d4dfcdb1a1b8573c9596d6f33
Patrick Gustafson <patrickgus@charter.net>
2019-12-26 11:26:04 -0800

    add test for sort function

--- test/sort.test.js
+++ test/sort.test.js
@@ -2,5 +2,10 @@ const sort = require("../index");
 const expect = require("chai").expect;
 
 describe("Sort function", () => {
-  it()
-})
+  it("should sort the array in ascending order", () => {
+    const arr1 = sort([3, 2, 4, 1]);
+    const arr2 = [1, 2, 3, 4];
+
+    expect(arr1).to.deep.equal(arr2);
+  });
+});
