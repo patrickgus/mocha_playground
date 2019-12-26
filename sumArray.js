d9c541357f2b41b4f400494c0e5296e273cce180
Patrick Gustafson <patrickgus@charter.net>
2019-12-26 13:16:58 -0800

    create sum function and tests

--- sumArray.js
+++ sumArray.js
@@ -1,7 +1,8 @@
 function sum(arr) {
   return new Promise((resolve, reject) => {
     const ans = arr.reduce((acc, curr) => {
-      return acc + curr;
+      const num = parseFloat(curr);
+      return acc + (isNaN(num) ? 0 : num);
     }, 0);
     resolve(ans);
   });
--- test/sumArray.test.js
+++ test/sumArray.test.js
@@ -5,4 +5,12 @@ describe("Array sum", () => {
   it("should return a promise", () => {
     expect(sum([1, 2, 3])).to.be.a("promise");
   });
+
+  it("should sum an array of numbers and non-numerical strings", () => {
+    return sum([1, "2", 3, "a"]).then(ans => {
+      expect(ans)
+        .to.be.a("number")
+        .that.equal(6);
+    });
+  });
 });
